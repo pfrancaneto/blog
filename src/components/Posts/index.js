c015429244abee05a141c05c19d5f28753fa2bff
Pedro França <pfranca.neto@gmail.com>
2022-11-09 17:58:01 -0300

    created post component - no responsive

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,10 +1,26 @@
-import './styles.css';
-
+import "./styles.css";
 
 const Post = () => {
   return (
-    <h1>Posts</h1>
+    <div className="post">
+      <div className="img-post"></div>
+      <h5 className="categoria">Tecnologia</h5>
+      <h3 className="title">O guia definitivo sobre o blog</h3>
+      <p className="desc-post">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium
+        maxime doloremque quia, sit ab iusto, eos laboriosam voluptas cum
+        tempora temporibus, magni autem nesciunt necessitatibus. Error eveniet
+        doloribus nesciunt qui!
+      </p>
+      <div className="profile">
+        <div className="avatar"></div>
+        <div className="desc-profile">
+          <h6>Fulano de Tal</h6>
+          <p>Aug 2, 2020 - 8 min react</p>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Post;
