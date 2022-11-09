0ecb799d471406b1af7117f2152c3c7d06ba0967
Pedro França <pfranca.neto@gmail.com>
2022-11-09 15:21:03 -0300

    developed header - not responsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,24 @@
-import './styles.css';
-
+import logoSvg from '../../svg/logo.svg';
+import "./styles.css";
 
 const Header = () => {
   return (
-    <h1>Header</h1>
+    <header>
+      <div className="logo">
+        <img src={logoSvg} alt="" />
+      </div>
+      <div className="search">
+        <input type="text" placeholder='Buscar artigos, destaques...'/>
+      </div>
+      <nav>
+        <ul className="menu">
+          <li><a className='link' href="/#">Categories</a></li>
+          <li><a className='link' href="/#">About</a></li>
+          <li><a className='link' href="/#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
   );
-}
+};
 
 export default Header;
